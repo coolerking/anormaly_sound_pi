5ee61fb5144806d4aff8638aae7144200c5cfca7
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2022-05-11 09:06:01 +0900

    .bashrc にenv/bin/activate記述がある想定

--- anormaly_sound_pi/record.sh
+++ anormaly_sound_pi/record.sh
@@ -2,7 +2,7 @@
 # crontab -e
 # 0-59 * * * * /home/pi/projects/anormaly_sound_pi/anormaly_sound_pi/record.sh
 export PATH=${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:.
-source ${HOME}/env/bin/activate
+#source ${HOME}/env/bin/activate
 cd ${HOME}/projects/anormaly_sound_pi/anormaly_sound_pi
 /home/pi/env/bin/python record.py --datadir data --age 5 --sec 1 --dev_index 1 --debug True
 exit $?
